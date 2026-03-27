e71223be9218a9b482d91dae1a7d481a3b3c9ef5
Aayush24bce10337 <aayush.24bce10337@vitbhopal.ac.in>
2026-03-27 23:11:58 +0530

    Update sir1.sh

--- sir1.sh
+++ sir1.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Your Name
+# Course: Open Source Software
 
+# -------- VARIABLES --------
+STUDENT_NAME="Your Name"
+SOFTWARE_CHOICE="Linux Kernel"
+
+# -------- SYSTEM INFO --------
+KERNEL=$(uname -r)
+DISTRO=$(grep '^PRETTY_NAME' /etc/os-release | cut -d= -f2 | tr -d '"')
+USER_NAME=$(whoami)
+HOME_DIR=$HOME
+UPTIME=$(uptime -p)
+DATE_TIME=$(date)
+
+# -------- DISPLAY --------
+echo "=========================================="
+echo " Open Source Audit — $STUDENT_NAME"
+echo "=========================================="
+echo "Software Chosen : $SOFTWARE_CHOICE"
+echo "Distribution    : $DISTRO"
+echo "Kernel Version  : $KERNEL"
+echo "User            : $USER_NAME"
+echo "Home Directory  : $HOME_DIR"
+echo "Uptime          : $UPTIME"
+echo "Date & Time     : $DATE_TIME"
+echo ""
+echo "License Info: Linux Kernel is licensed under GNU GPL v2"
+echo "=========================================="
