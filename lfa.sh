f5ef989b63243b3590af32118f21501e21c60169
Aayush24bce10337 <aayush.24bce10337@vitbhopal.ac.in>
2026-03-30 19:05:06 +0530

    Update lfa.sh

--- lfa.sh
+++ lfa.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 # Script 4: Log File Analyzer
-
-# Usage: ./script4_log_analyzer.sh /var/log/syslog error
+# Author: Aayush Bhargava
 
 LOGFILE=$1
 KEYWORD=${2:-"error"}
 COUNT=0
 
-# -------- CHECK FILE --------
 if [ ! -f "$LOGFILE" ]; then
-    echo "Error: File $LOGFILE not found."
+    echo "Error: File not found."
     exit 1
 fi
 
-# -------- EMPTY FILE CHECK --------
 if [ ! -s "$LOGFILE" ]; then
-    echo "File is empty. Retrying in 2 seconds..."
-    sleep 2
+    echo "File is empty. Exiting..."
+    exit 1
 fi
 
-# -------- READ FILE --------
 while IFS= read -r LINE; do
     if echo "$LINE" | grep -iq "$KEYWORD"; then
         COUNT=$((COUNT + 1))
     fi
 done < "$LOGFILE"
 
-# -------- OUTPUT --------
-echo "----------------------------------------"
-echo "Keyword '$KEYWORD' found $COUNT times in $LOGFILE"
+echo "Keyword '$KEYWORD' found $COUNT times."
 
 echo ""
 echo "Last 5 matching lines:"
