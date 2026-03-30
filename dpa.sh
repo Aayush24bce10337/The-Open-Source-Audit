81576c053d6e43edd5db272bf20d6c0c8340bfd7
Aayush24bce10337 <aayush.24bce10337@vitbhopal.ac.in>
2026-03-30 19:04:22 +0530

    Update dpa.sh

--- dpa.sh
+++ dpa.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 # Script 3: Disk and Permission Auditor
+# Author: Aayush Bhargava
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 
 echo "Directory Audit Report"
-echo "----------------------------------------"
+echo "----------------------"
 
-# Loop through directories
 for DIR in "${DIRS[@]}"; do
     if [ -d "$DIR" ]; then
-        # Get permissions, owner, group
         PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
-        
-        # Get directory size
         SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
-
         echo "$DIR => Permissions: $PERMS | Size: $SIZE"
     else
         echo "$DIR does not exist"
@@ -22,13 +18,10 @@ for DIR in "${DIRS[@]}"; do
 done
 
 echo ""
-echo "Checking Linux Kernel config directory..."
+echo "Checking Git configuration directory..."
 
-# Kernel config location
-KERNEL_CONFIG="/boot"
-
-if [ -d "$KERNEL_CONFIG" ]; then
-    ls -ld $KERNEL_CONFIG
+if [ -d "$HOME/.git" ]; then
+    ls -ld $HOME/.git
 else
-    echo "Kernel config directory not found."
+    echo "No global .git directory found in home"
 fi
