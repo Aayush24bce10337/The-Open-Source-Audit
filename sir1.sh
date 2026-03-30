a3e2e2f8f12da262055269af85f470455b8ee55d
Aayush24bce10337 <aayush.24bce10337@vitbhopal.ac.in>
2026-03-30 19:03:16 +0530

    Update sir1.sh

--- sir1.sh
+++ sir1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Script 1: System Identity Report
-# Author: Your Name
-# Course: Open Source Software
+# Author: Your Name | Course: Open Source Software
 
 # -------- VARIABLES --------
 STUDENT_NAME="Your Name"
-SOFTWARE_CHOICE="Linux Kernel"
+SOFTWARE_CHOICE="Git"
 
 # -------- SYSTEM INFO --------
 KERNEL=$(uname -r)
+OS=$(uname -s)
 DISTRO=$(grep '^PRETTY_NAME' /etc/os-release | cut -d= -f2 | tr -d '"')
 USER_NAME=$(whoami)
 HOME_DIR=$HOME
@@ -20,6 +20,7 @@ echo "=========================================="
 echo " Open Source Audit — $STUDENT_NAME"
 echo "=========================================="
 echo "Software Chosen : $SOFTWARE_CHOICE"
+echo "Operating System: $OS"
 echo "Distribution    : $DISTRO"
 echo "Kernel Version  : $KERNEL"
 echo "User            : $USER_NAME"
@@ -27,5 +28,5 @@ echo "Home Directory  : $HOME_DIR"
 echo "Uptime          : $UPTIME"
 echo "Date & Time     : $DATE_TIME"
 echo ""
-echo "License Info: Linux Kernel is licensed under GNU GPL v2"
+echo "License Info: Linux is licensed under GNU GPL v2"
 echo "=========================================="
